64caa1e038fbd42d6517807ca6aef4fd17e08676
ignacio <nachomattos78@gmail.com>
2019-10-17 12:51:23 +0100

    banner&logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from "./Header";
 
 import './App.css';
 
 function App() {
   return (
     <div className="container">
+      <Header />
+      
       
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Header.css"
+import logo from "./market-logo.png";
+
+class Header extends React.Component{
+    render() {
+        
+        return (
+            <div className="col-12 top-image">
+            <img className="logo" src={logo} alt="logo"></img>
+            </div> 
+
+        )
+    }
+}
+
+export default Header;
