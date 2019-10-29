207caf1bed42691142d38ec89c6662b4a838772f
ignacio <nachomattos78@gmail.com>
2019-10-29 20:17:03 +0000

    logo mobile

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,15 @@
 import React, {Component} from "react"
 import "./Header.css"
-import logo from "./images/market-logo.png";
+
 
 class Header extends Component{
     render() {
         
         return (
             <div className="col- top-image">
-            <img className="logo" src={logo} alt="logo" />
+            <div className="logo image"></div>
             </div> 
+
 
         )
     }
