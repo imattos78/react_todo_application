ece990ffe18fbbc8d607a5e8d59408ab6219bedc
ignacio <nachomattos78@gmail.com>
2019-12-07 23:40:57 +0000

    mobile screen modif

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -18,10 +18,10 @@ class Item extends Component {
                 <div className="col-1 arrow-align">
                     <p> <i className="fas fa-angle-right arrow"></i></p>
                 </div>
-                <div className={!this.props.bought ? "col-3 col-sm-1 d-flex align-self-center text-product-buy" : "d-none d-md-block"}>
+                <div className={!this.props.bought ? "col-4 col-sm-2 d-flex align-self-center text-product-buy" : "d-none d-md-block"}>
                     <p>{!this.props.bought && this.props.text}</p>
                 </div>
-                <div className={this.props.bought ? "col-3 col-sm-2":"col-3 col-sm-2 d-flex justify-content-center"}>
+                <div className={this.props.bought ? "col-4 col-sm-2":"col-2 col-sm-2 d-flex justify-content-center"}>
                     <p> {this.props.bought ? this.props.text : `Qty: ${this.props.quantity}`}</p>
                 </div>
 
